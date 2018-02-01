8085e575a22eeea94b2d77da7d444d7bb9fa153b
= <gsardana001@gmail.com>
2018-02-02 02:19:52 +0530

    clientServer added

--- clientServer.py
+++ clientServer.py
@@ -1,5 +1,6 @@
 import socket
 import tkinter
+import threading
 host=input("Enter host: ")
 port=input("Enter port: ")
 if not port:
@@ -16,3 +17,34 @@ def receive():
             msg_list.insert(tkinter.END, msg)
         except OSError:
             break
+def send():
+    msg =my_msg.get()
+    my_msg.set("")
+    s.send(bytes(msg, "utf-8"))
+    if msg== "{quit}":
+        s.close()
+        top.quit()
+def close():
+    s.close()
+    top.quit()
+    exit()
+top=tkinter.Tk()
+top.title("Private Chat Room")
+msg_frame = tkinter.Frame(top)
+my_msg = tkinter.StringVar()
+my_msg.set("Type your name here")
+scrollbar=tkinter.Scrollbar(msg_frame)
+msg_list=tkinter.Listbox(msg_frame, height=50, width=300, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+msg_list.pack(side=tkinter.LEFT, fill = tkinter.BOTH)
+msg_list.pack()
+msg_frame.pack()
+entry_field=tkinter.Entry(top, textvariable=my_msg)
+entry_field.bind("<Return>", send)
+send_button= tkinter.Button(top, text="Send", command=send)
+quit_button=tkinter.Button(top, text="QUIT", commmand=close)
+send_button.pack()
+quit_button.pack()
+receive_thread=threading.Thread(target=receive)
+receive_thread.start()
+tkinter.mainloop()
